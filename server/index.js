a484d556d47d370cb26f4c8aa4c9ee7522367cc6
asimsinan <asimyuksel@sdu.edu.tr>
2024-06-25 16:11:53 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -13,8 +13,17 @@ const {notFound,errorHandler} = require('./middleware/errorMiddleware')
 
 
 const app = express();
+var allowCrossDomain = function(req, res, next) { 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    }
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
+app.use(allowCrossDomain);
 app.use(upload())
 app.use('/uploads',express.static(__dirname + '/uploads'))
 
